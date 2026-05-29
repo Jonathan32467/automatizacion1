c0fbec51dd8e8bebbd29c248249a092dbf4b630d
Jonathan32467 <al202532467@plataforma-utslp.net>
2026-05-29 13:50:15 -0600

    29-05-2026 1

---
+++ segundo_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+path=carpeta2
+file=archivo2.log
+ruta=$path/$file
+
+if [[ -f "path" ]] then
+mkdir $path
+if [[ -f "ruta" ]] then
+touch $ruta
+
+while true; do
+  fecha=$(date '+%Y-%m-%d %H:%M:%S')
+  echo $fecha >> $ruta
+  sleep 5
+done
